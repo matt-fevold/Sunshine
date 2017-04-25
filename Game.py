04de3ab341c9e079a112923a82bc54f3eea3cc46
Matthew Fevold <matt.fevold@gmail.com>
2017-04-24 19:37:25 -0500

    learning pygame

--- Game.py
+++ Game.py
@@ -1,5 +1,52 @@
 #play game
+import sys
 
+import pygame
+
+
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+surface = pygame.Surface((100, 100))
+done = False
+is_blue = True
+x = 30
+y = 30
+
+clock = pygame.time.Clock()
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                        is_blue = not is_blue
+
+
+        
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_1]:
+            done = True
+            pygame.display.quit()
+            pygame.quit()
+            sys.exit()
+        else:
+            
+
+            if pressed[pygame.K_UP]: y -= 3
+            if pressed[pygame.K_DOWN]: y += 3
+            if pressed[pygame.K_LEFT]: x -= 3
+            if pressed[pygame.K_RIGHT]: x += 3
+            
+            
+            screen.fill((0, 0, 0))
+            if is_blue: color = (0, 128, 255)
+            else: color = (255, 100, 0)
+            pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+            
+            pygame.display.flip()
+            clock.tick(60)
 
 
 def readInWorld(filepath):
@@ -28,6 +75,6 @@ def graph(World):
         print()
 
 
-world = readInWorld("C:\\tools\\test.txt")
+#world = readInWorld("C:\\tools\\test.txt")
 #print(world[0])
-graph(world)
+#graph(world)
