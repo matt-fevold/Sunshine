d1cb1c555d5a3ee730445c89c7e3c30fa713ada9
Matthew Fevold <matt.fevold@gmail.com>
2017-04-25 01:13:27 -0500

    added crazy lights and more controls
    still playing with pygame, no real work on project yet

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 #play game
 import sys
+import random
 
 import pygame
 
@@ -13,16 +14,27 @@ done = False
 is_blue = True
 x = 30
 y = 30
+red = 0
+green = 0 
+blue = 0
+box_x =25
+box_y =25
 
 clock = pygame.time.Clock()
+
+def randomInt():
+	return random.randrange(255)
+
 
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                        done = True
+                    done = True
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                         is_blue = not is_blue
-
+                        red = randomInt()
+                        green = randomInt()
+                        blue = randomInt()
 
         
         pressed = pygame.key.get_pressed()
@@ -34,47 +46,24 @@ while not done:
         else:
             
 
-            if pressed[pygame.K_UP]: y -= 3
-            if pressed[pygame.K_DOWN]: y += 3
-            if pressed[pygame.K_LEFT]: x -= 3
-            if pressed[pygame.K_RIGHT]: x += 3
-            
+            if pressed[pygame.K_UP]: y -= 5
+            if pressed[pygame.K_DOWN]: y += 5
+            if pressed[pygame.K_LEFT]: x -= 5
+            if pressed[pygame.K_RIGHT]: x += 5
+            if pressed[pygame.K_s]: box_y -= 2
+            if pressed[pygame.K_w]: box_y += 2
+            if pressed[pygame.K_a]: box_x -=2
+            if pressed[pygame.K_d]: box_x +=2
             
             screen.fill((0, 0, 0))
-            if is_blue: color = (0, 128, 255)
-            else: color = (255, 100, 0)
-            pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+            if is_blue: color = (red, green, blue)
+            else: color = (randomInt(), randomInt(), randomInt())
+            pygame.draw.rect(screen, color, pygame.Rect(x, y, box_x, box_y))
             
             pygame.display.flip()
             clock.tick(60)
 
 
-def readInWorld(filepath):
-    tempWorld = []
-    World = []
-    
-    with open(filepath,"r") as file:
-        for x in file.readline():
-            tempWorld.append(file)
 
-    #print (tempWorld[0])
-            
-    return World
-        
-
-
-
-def graph(World):
-    print(" \n=" , len(World))
-    for y in reversed(range(max(World))):
-        for x in range(len(World)):
-            if(World[x]>y):
-                print("X",end="")
-            else:
-                print(" ",end="")
-        print()
-
-
-#world = readInWorld("C:\\tools\\test.txt")
-#print(world[0])
-#graph(world)
+			
+			
